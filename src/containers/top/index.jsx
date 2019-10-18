cd2ad16c9147438fef9e8d24536097d87b92dd05
b7026 <B7026@oic.jp>
2019-10-18 17:15:25 +0900

    Add top

--- src/containers/top/index.jsx
+++ src/containers/top/index.jsx
@@ -5,6 +5,13 @@ export const Top = () => {
     <div>
       <h1>Menstagram</h1>
       <p>トップ</p>
+      <h1>SUSURU FOREVER,</h1>
+      <h1>SUSURU ANYWHERE.</h1>
+      <p>Menstagramは世界中のラーメンコミュニティを支える特化型SNSです。</p>
+      
+      
+      <p>アカウントを作成しますか？<a src href="screen/e11796ba-5a84-4055-8428-9e7cc6702eab/-register"><br></br>登録する</a></p>
+      <p>アカウントをお持ちですか？<a src href="screen/71dd1a93-a9ef-46c5-809b-20f962b736be/-login"><br></br>ログインする</a></p>
     </div>
   );
 };
