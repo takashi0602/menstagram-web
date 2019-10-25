1e827e108b0e4ec86e2d26534489fa95f0a60af7
takashi0602 <takashioicsn@gmail.com>
2019-10-25 15:53:24 +0900

    modify top, add styled.jsx

--- src/containers/top/index.jsx
+++ src/containers/top/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import titleSvg from "../../assets/images/title.svg";
+import { SubTitle } from "./styled";
 
 export const Top = () => {
   return (
     <div className="c-container__padding">
       <div className="text-center pt-5 mb-3">
-        <img src={titleSvg} alt="Menstagram" className="mb-5"/>
-        <h3>SUSURU FOREVER,<br />SUSURU ANYWHERE.</h3>
+        <img src={titleSvg} alt="Menstagram" className="mb-3"/>
+        <div className="mb-3">
+          <img src="http://placehold.it/100x100/?text=Icon" alt="MenstagramIcon"/>
+        </div>
+        <SubTitle>SUSURU FOREVER,<br />SUSURU ANYWHERE.</SubTitle>
       </div>
       <p className="mb-5">Menstagramは世界中のラーメンコミュニティを支える特化型SNSです。</p>
       <div className="mb-3">
