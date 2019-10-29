3c981de04ed66f8c4993b474a1f8722e8841f46f
takashi0602 <takashioicsn@gmail.com>
2019-10-29 23:53:21 +0900

    modify login container

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import titleSvg from "../../assets/images/title.svg";
+import { Relative, Absolute, Eye } from "./styled";
 
-export const Login = () => {
-  return (
-    <div>
-      <h1>Menstagram</h1>
-      <p>ログイン</p>
-    </div>
-  );
-};
+export class Login extends Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      iconName: faEye
+    }
+  }
+
+  render() {
+    const showPassword = () => {
+      this.state.iconName === faEye
+        ? this.setState((state)=>({
+          iconName: faEyeSlash
+        }))
+        : this.setState((state)=>({
+          iconName: faEye
+        }));
+    };
+
+    const checkInputType = () => {
+      return this.state.iconName === faEye ? 'password' : 'text';
+    };
+
+    return (
+      <div className="c-container__padding">
+        <div className="text-center pt-5 mb-5">
+          <img src={titleSvg} alt="Menstagram" />
+        </div>
+        <div className="mb-4">
+          <input type="text" className="c-form mb-3" placeholder="ユーザーID" />
+          <Relative>
+            <input type={checkInputType()} className="c-form" placeholder="パスワード" />
+            <Absolute onClick={showPassword}>
+              <FontAwesomeIcon icon={this.state.iconName} style={Eye} />
+            </Absolute>
+          </Relative>
+        </div>
+        <div className="mb-5">
+          <button className="c-button__orange w-100">登録</button>
+        </div>
+        <div className="mb-3">
+          <p className="mb-0">アカウントを作成しますか？</p>
+          <Link to="/login">登録する</Link>
+        </div>
+        <Link to="/">トップへ戻る</Link>
+      </div>
+    );
+  }
+}
---
+++ src/containers/login/styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Relative = styled.div`position: relative`;
+
+export const Absolute = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 10px;
+  height: 40px;
+`;
+
+export const Eye = {
+  display: 'inline-block',
+  width: '40px',
+  height: '40px',
+  background: '#C6C6C6',
+  color: '#666666',
+  cursor: 'pointer',
+  padding: '5px',
+  borderRadius: '5px',
+};
