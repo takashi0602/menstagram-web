ec52b9ef3eaa9a2a2fa40933c22c3cd53e557312
takashi0602 <takashioicsn@gmail.com>
2019-11-01 14:18:07 +0900

    add test. modify top page.

--- src/containers/top/index.jsx
+++ src/containers/top/index.jsx
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import titleSvg from "../../assets/images/title.svg";
 import { SubTitle } from "./styled";
 
-export const Top = () => {
-  return (
-    <div className="c-container__padding">
-      <div className="text-center pt-5 mb-3">
-        <img src={titleSvg} alt="Menstagram" className="mb-3"/>
-        <div className="mb-3">
-          <img src="http://placehold.it/100x100/?text=Icon" alt="MenstagramIcon"/>
+export class Top extends Component {
+  render() {
+    return (
+      <div className="c-container__padding">
+        <div className="text-center pt-5 mb-3">
+          <img src={titleSvg} alt="Menstagram" className="mb-3"/>
+          <div className="mb-3">
+            <img src="http://placehold.it/100x100/?text=Icon" alt="MenstagramIcon"/>
+          </div>
+          <SubTitle>SUSURU FOREVER,<br />SUSURU ANYWHERE.</SubTitle>
         </div>
-        <SubTitle>SUSURU FOREVER,<br />SUSURU ANYWHERE.</SubTitle>
+        <p className="mb-5">Menstagramは世界中のラーメンコミュニティを支える特化型SNSです。</p>
+        <div className="mb-3">
+          <p className="mb-0">アカウントを作成しますか？</p>
+          <Link to="/register">登録する</Link>
+        </div>
+        <div>
+          <p className="mb-0">アカウントをお持ちですか？</p>
+          <Link to="/login">ログインする</Link>
+        </div>
       </div>
-      <p className="mb-5">Menstagramは世界中のラーメンコミュニティを支える特化型SNSです。</p>
-      <div className="mb-3">
-        <p className="mb-0">アカウントを作成しますか？</p>
-        <Link to="/register">登録する</Link>
-      </div>
-      <div>
-        <p className="mb-0">アカウントをお持ちですか？</p>
-        <Link to="/login">ログインする</Link>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
