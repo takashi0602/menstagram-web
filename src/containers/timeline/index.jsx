7ecfd71f25bf2c1937334cbbb004cec3479514af
suzuking1125 <b7165@oic.jp>
2019-11-12 15:04:12 +0900

    timeline

--- src/containers/timeline/index.jsx
+++ src/containers/timeline/index.jsx
@@ -3,12 +3,25 @@ import React, { Component } from 'react';
 export class Timeline extends Component {
   render() {
     return (
-      <div className="c-container__padding">
+      <div className="c-container__padding" >
+        <div class="containner" >
         <p>これはタイムラインです</p>
+            <div class="row justify-content-center">
+            <button class="btn-lg active rounded-pill" onClick={()=>{}}>
+              グローバル
+              </button>
+            
+            <button class="btn-lg active rounded-pill" onClick={()=>{}}>
+              プライベート
+              </button>
+            </div>
+          </div>
       </div>
+/*投稿内容部分はcomponentsにかくのかな？*/
+      
     );
   }
-}         
+}        
 
        
       
