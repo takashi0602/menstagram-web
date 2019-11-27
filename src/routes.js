0fea4cf9cc8102fcb0e66a90ceb65439fa72b395
kakimotoShunya <b7080@oic.jp>
2019-11-28 01:07:19 +0900

    add フォロー/フォロワ―画面の見た目作成

--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,21 @@ import { ConnectedRouter } from 'connected-react-router';
 import { history } from './history';
 import { store } from './store/index';
 import { Top } from './containers/top';
-import { Register } from "./containers/register";
-import { Login } from "./containers/login";
+import { Register } from './containers/register';
+import { Login } from './containers/login';
+import { Follow } from './containers/follow';
 
 export const Routes = () => {
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <div className='c-container'>
+        <div className="c-container">
           <Switch>
-            <Route exact path={'/'} component={ Top } />
-            <Route exact path={'/register'} component={ Register } />
-            <Route exact path={'/login'} component={ Login } />
+            <Route exact path={'/'} component={Top} />
+            <Route exact path={'/register'} component={Register} />
+            <Route exact path={'/login'} component={Login} />
+            <Route exact path={'/follow'} component={Follow} />
+            <Route exact path={'/user/:id/follow'} component={Follow} />
           </Switch>
         </div>
       </ConnectedRouter>
