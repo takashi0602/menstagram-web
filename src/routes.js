3bc6ab27f11b7c82c4881a9c95a58fae0ec22fb1
murato <b7350@oic.jp>
2019-12-03 22:35:39 +0900

    通知mock

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { history } from './history';
 import { store, persistor } from './store/index';
 import { Top } from './containers/top';
-<<<<<<< HEAD
 import { Register } from './containers/register';
 import { Login } from './containers/login';
 import { Logout } from './containers/logout';
@@ -16,16 +15,10 @@ import { Management } from './containers/notification/management';
 
 
 
-=======
-import { Register } from "./containers/register";
-import { Login } from "./containers/login";
-import { Notification } from "./containers/notification";
->>>>>>> 70a55ee25630e0ec490f5e57e5af3047bfa05311
 
 export const Routes = () => {
   return (
     <Provider store={store}>
-<<<<<<< HEAD
       <PersistGate loading={null} persistor={persistor}>
         <ConnectedRouter history={history}>
           <div className="c-container">
@@ -42,18 +35,6 @@ export const Routes = () => {
           </div>
         </ConnectedRouter>
       </PersistGate>
-=======
-      <ConnectedRouter history={history}>
-        <div className='c-container'>
-          <Switch>
-            <Route exact path={'/'} component={ Top } />
-            <Route exact path={'/register'} component={ Register } />
-            <Route exact path={'/login'} component={ Login } />
-            <Route exact path={'/notification'} component={ Notification }/>
-          </Switch>
-        </div>
-      </ConnectedRouter>
->>>>>>> 70a55ee25630e0ec490f5e57e5af3047bfa05311
     </Provider>
   );
 };
