fdffc0e5b2860468f921bb3d1e74ae2e9ab65839
kakimotoShunya <b7080@oic.jp>
2019-12-20 16:24:32 +0900

    modify follow button pading

--- src/components/notice/styled.jsx
+++ src/components/notice/styled.jsx
@@ -27,9 +27,10 @@ export const UnfollowButton = styled.button`
   height: 30px;
   color: #3c3c3c;
   background-color: white;
-  display; inline-block;
+  display: inline-block;
   border: #c6c6c6 1px solid;
   border-radius: 50rem;
+  padding: 7.5px;
 `;
 
 export const FollowButton = styled.button`
@@ -39,8 +40,9 @@ export const FollowButton = styled.button`
   height: 30px;
   color: white;
   background-color: #eb6101;
-  display; inline-block;
+  display: inline-block;
   border-radius: 50rem;
+  padding: 7.5px;
 `;
 
 export const Calc = styled.div`
