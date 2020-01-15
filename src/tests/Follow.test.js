5f441c47e49829124d8ab37be298e55c64745962
takashi0602 <takashioicsn@gmail.com>
2020-01-16 00:30:27 +0900

    modify Follow test

--- src/tests/Follow.test.js
+++ src/tests/Follow.test.js
@@ -2,19 +2,33 @@ import { getFollowed } from '../api/follow/followed';
 import { getFollowing } from '../api/follow/following';
 import { requestPostDetail } from '../api/postDetail';
 import { requestRegister } from '../api/auth';
+import { postFollow, postUnfollow } from '../api/follow';
 
 jest.setTimeout(10000);
 
-const random = Math.random()
+const random1 = Math.random()
   .toString(36)
   .slice(-8);
 
-const registerData = {
+const random2 = Math.random()
+  .toString(36)
+  .slice(-8);
+
+const registerData1 = {
   payload: {
-    user_id: random,
-    screen_name: random,
-    email: `${random}@gmail.com`,
-    password: random
+    user_id: random1,
+    screen_name: random1,
+    email: `${random1}@gmail.com`,
+    password: random1
+  }
+};
+
+const registerData2 = {
+  payload: {
+    user_id: random2,
+    screen_name: random2,
+    email: `${random2}@gmail.com`,
+    password: random2
   }
 };
 
@@ -32,8 +46,13 @@ const followData = {
   }
 };
 
+const followUnfollowData = {
+  accessToken: '',
+  targetUserId: random1
+};
+
 it('request following api', async () => {
-  const registerRes = await requestRegister(registerData);
+  const registerRes = await requestRegister(registerData1);
   postData.accessToken = registerRes.response.data.access_token;
   followData.accessToken = registerRes.response.data.access_token;
   expect(registerRes.response.statusText).toEqual('OK');
@@ -49,4 +68,17 @@ it('request following api', async () => {
 it('request followed api', async () => {
   const followedRes = await getFollowed(followData);
   expect(followedRes.response.status).toEqual(200);
+});
+
+it('request follow api', async () => {
+  const registerRes = await requestRegister(registerData2);
+  followUnfollowData.accessToken = registerRes.response.data.access_token;
+
+  const followRes = await postFollow(followUnfollowData);
+  expect(followRes.response.status).toEqual(200);
+});
+
+it('request unfollow api', async () => {
+  const unfollowRes = await postUnfollow(followUnfollowData);
+  expect(unfollowRes.response.status).toEqual(200);
 });
