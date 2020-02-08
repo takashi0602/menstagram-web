dcbd5e30be05aad30ed6e56f4c2672a2f568fd0f
takashi0602 <takashioicsn@gmail.com>
2020-02-09 02:14:16 +0900

    add InitErrorComponent

---
+++ src/components/error/init.jsx
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import connect from 'react-redux/es/connect/connect';
+import { notError } from "../../actions/error";
+
+class InitErrorComponent extends Component {
+  state = {
+    location: null
+  };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log(props.location);
+    if(props.location !== state.location) {
+      props.initError();
+      return {
+        location: props.location
+      };
+    }
+    return null;
+  }
+
+  render() {
+    return null;
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    initError() {
+      dispatch(notError());
+    }
+  };
+}
+
+export const InitError = connect(
+  null,
+  mapDispatchToProps
+)(InitErrorComponent);
+
+InitErrorComponent.propTypes = {
+  initError: PropTypes.func
+};
